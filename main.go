cc581aa8132dfe09e87401aa4160f6568f603e4c
Aurélien Chabot <aurelien@chabot.fr>
2018-05-08 16:22:34 +1000

    Code drop of initial implementation

---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/odwrtw/transmission"
+)
+
+const (
+	OUT = "."
+	URL = "127.0.0.1:9091"
+)
+
+var IGNORE = [...]string{"the", "and", "an", "a"}
+var EXTENSION = [...]string{".mp4", ".mkv", ".avi"}
+
+func contains(slice []string, search string) bool {
+	for _, value := range slice {
+		if value == search {
+			return true
+		}
+	}
+	return false
+}
+
+func splitter(s string, splits string) []string {
+	m := make(map[rune]int)
+	for _, r := range splits {
+		m[r] = 1
+	}
+
+	splitter := func(r rune) bool {
+		return m[r] == 1
+	}
+
+	return strings.FieldsFunc(s, splitter)
+}
+
+func findMatch(torrent string) (string, error) {
+	torrentName := strings.ToLower(torrent)
+
+	files, err := ioutil.ReadDir(OUT)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	match := make(map[string]int)
+
+	for _, f := range files {
+		if f.IsDir() {
+			s := splitter(strings.ToLower(f.Name()), ". ")
+			count := 0
+			for _, ss := range s {
+				if !contains(IGNORE[:], ss) {
+					if strings.Contains(torrentName, ss) {
+						count = count + 1
+					} else {
+						count = -1
+						break
+					}
+				}
+			}
+			if count > 0 {
+				match[f.Name()] = count
+			}
+		}
+	}
+
+	if len(match) == 0 {
+		return "", fmt.Errorf("no match found for %s", torrent)
+	}
+
+	max_count := 0
+	max_name := ""
+
+	for name, count := range match {
+		if count > max_count {
+			max_count = count
+			max_name = name
+		}
+	}
+
+	fmt.Printf("Found match %s", max_name)
+	fmt.Println("")
+
+	return max_name, nil
+}
+
+func moveFile(file string, to string) error {
+	if contains(EXTENSION[:], path.Ext(file)) {
+		fmt.Printf("Moving %s to %s\n", file, to)
+		return os.Rename(file, path.Join(OUT, to, path.Base(file)))
+	}
+	fmt.Printf("Skipping %s\n", file)
+	return nil
+}
+
+func move(torrentDir string, to string) error {
+	f, err := os.Stat(torrentDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if f.Mode().IsDir() {
+		files, err := ioutil.ReadDir(torrentDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, f := range files {
+			err := moveFile(path.Join(torrentDir, f.Name()), to)
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		err := moveFile(torrentDir, to)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("Filtering...")
+
+	conf := transmission.Config{
+		Address: fmt.Sprintf("http://%s/transmission/rpc", URL),
+	}
+	t, err := transmission.New(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	torrents, err := t.GetTorrents()
+	if err != err {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d torrents found\n", len(torrents))
+	for _, torrent := range torrents {
+		if torrent.IsFinished {
+			fmt.Println()
+			fmt.Printf("Torrent : %s\n", torrent.Name)
+			match, err := findMatch(torrent.Name)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			torrentDir := path.Join(torrent.DownloadDir, torrent.Name)
+			err = move(torrentDir, match)
+			if err != nil {
+				fmt.Printf("Error moving file from torrent %s", torrent.Name)
+				continue
+			}
+			t.RemoveTorrents([]*transmission.Torrent{torrent}, true)
+		}
+	}
+}
